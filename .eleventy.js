d8f6de102dd1db72255a9528b5c2471bfa53c6e4
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-03-26 19:40:03 +0100

    add css

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/**/*.css");
+
   return {
     dir: {
       input: "src",
+      passthroughFileCopy: true,
     },
   };
 };
