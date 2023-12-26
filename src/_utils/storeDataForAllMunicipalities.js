7eff578538868be03a32e676ab45d3aecfd7b379
Sindre Bøyum <sindre.boyum@tietoevry.com>
2023-12-26 20:54:09 +0100

    fix: import with file extension

--- src/_utils/storeDataForAllMunicipalities.js
+++ src/_utils/storeDataForAllMunicipalities.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { existsSync, readFileSync, writeFileSync } from "node:fs";
-import allMunicipalities from "./allMunicipalities";
-import getCarbonDataForWebsite from "./getCarbonDataForWebsite";
+import allMunicipalities from "./allMunicipalities.js";
+import getCarbonDataForWebsite from "./getCarbonDataForWebsite.js";
 
 const getPath = (/** @type {string} */ isoDate) => `data/${isoDate}.json`;
 
@@ -72,21 +72,27 @@ const mergeMunicipalitiesData = (newData, existingData) => {
     .filter(Boolean);
 };
 
+const getISODate = (/** @type {Date} */ date) =>
+  date.toISOString().split("T")[0];
+
 const run = async () => {
-  const today = new Date().toISOString().split("T")[0];
-  const yesterday = new Date(new Date().getTime() - 24 * 60 * 60 * 1000)
-    .toISOString()
-    .split("T")[0];
+  const today = new Date();
+  const yesterday = new Date(today.getTime() - 24 * 60 * 60 * 1000);
 
-  const path = getPath(today);
+  const todayISO = getISODate(today);
+  const yesterdayISO = getISODate(yesterday);
 
-  const previousData = existsSync(getPath(yesterday))
-    ? JSON.parse(readFileSync(getPath(yesterday)).toString("utf-8"))
+  const path = getPath(todayISO);
+
+  const previousData = existsSync(getPath(yesterdayISO))
+    ? JSON.parse(readFileSync(getPath(yesterdayISO)).toString("utf-8"))
     : [];
   const newData = await fetchDataForAllMunicipalities();
   if (newData?.length === 1) {
     throw new Error("No new data found for any municipality");
   }
+
+  console.log(newData);
 
   const mergedData = mergeMunicipalitiesData(newData, previousData);
   const sortedData = mergedData.sort(
