b01581e3fbaff85c9718d83ee996576dc0cb9c3c
Sindre Bøyum <sindre.boyum@tietoevry.com>
2024-03-23 13:13:50 +0100

    chore: fallback to data from latest date

--- src/_data/getDataForAllMunicipalities.js
+++ src/_data/getDataForAllMunicipalities.js
@@ -7,26 +7,33 @@ const getPath = (/** @type {string} */ isoDate) => `data/${isoDate}.json`;
 const path = getPath(today);
 const html = String.raw;
 
+const latestDate = "2023-12-26";
+
 export default async function getDataForAllMunicipalities() {
   const data = existsSync(path)
     ? JSON.parse(readFileSync(path).toString("utf-8"))
-    : [];
+    : JSON.parse(readFileSync(getPath(latestDate)).toString("utf-8"));
 
-  return data.map((municipalityData, index) => {
-    return html`<tr>
-      <td>${index + 1}</td>
-      <td><a href="${municipalityData.url}">${municipalityData.name}</a></td>
-      <td>
-        ${(municipalityData.statistics.co2.renewable.grams ?? 0).toFixed(2)}g /
-        ${(municipalityData.statistics.co2.grid.grams ?? 0).toFixed(2)}g
-      </td>
-      <td>
-        ${(
-          ((municipalityData.statistics.co2.grid.grams ?? 0) * 100) /
-          1.76
-        ).toFixed(2)}%
-      </td>
-      <td>${(municipalityData.bytes / 1_000_000).toFixed(2)}MB</td>
-    </tr>`;
-  });
+  return data.map(
+    (
+      /** @type {{ url: string; name: string; statistics: { co2: { renewable: { grams: number; }; grid: { grams: number; }; }; }; bytes: number; }} */ municipalityData,
+      /** @type {number} */ index,
+    ) => {
+      return html`<tr>
+        <td>${index + 1}</td>
+        <td><a href="${municipalityData.url}">${municipalityData.name}</a></td>
+        <td>
+          ${(municipalityData.statistics.co2.renewable.grams ?? 0).toFixed(2)}g
+          / ${(municipalityData.statistics.co2.grid.grams ?? 0).toFixed(2)}g
+        </td>
+        <td>
+          ${(
+            ((municipalityData.statistics.co2.grid.grams ?? 0) * 100) /
+            1.76
+          ).toFixed(2)}%
+        </td>
+        <td>${(municipalityData.bytes / 1_000_000).toFixed(2)}MB</td>
+      </tr>`;
+    },
+  );
 }
